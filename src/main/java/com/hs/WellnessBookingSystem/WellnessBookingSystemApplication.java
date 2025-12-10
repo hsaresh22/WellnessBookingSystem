185eb4ec2a510f72e494ce2fc973cfe3af0feeec
hsaresh22 <hari.saresh@gmail.com>
2025-12-11 02:36:40 +0800

    Initial commit

---
+++ src/main/java/com/hs/WellnessBookingSystem/WellnessBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hs.WellnessBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hs/WellnessBookingSystem/WellnessBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.WellnessBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
